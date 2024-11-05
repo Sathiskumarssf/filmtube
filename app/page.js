d3bd5490ae87be2db18a9019c3eead9a751045a7
Educative Learner <sathiskumarsk2001@gmail.com>
2024-11-06 00:26:52 +0530

    this web application is for vidios sharing web application

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="bg-gray-200 text-3xl font-bold">This is the first</h1>
+      <h2 className="bg-gray-200 text-2xl">This is the first page for FilmTube</h2>
+    </div>
+  );
+}
